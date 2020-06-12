2fecbfeeef5e936fffec2ab9662f039a4b30d50c
tzickel <tzickel@users.noreply.github.com>
2020-06-12 22:47:08 +0300

    omw

--- justredis/nonsync/environment.py
+++ justredis/nonsync/environment.py
@@ -1,7 +1,18 @@
-from .environments.anyio import AnyIOEnvironment
+AnyIOEnvironment = None
+TrioEnvironment = None
 
 
-def get_environment(environment=AnyIOEnvironment, **kargs):
-    if environment == "anyio":
-        environment = AnyIOEnvironment
-    return environment
+def get_environment(environment=None, **kargs):
+    global AnyIOEnvironment, TrioEnvironment
+
+    if not environment or environment == "anyio":
+        if not AnyIOEnvironment:
+            from .environments.anyio import AnyIOEnvironment
+        env = AnyIOEnvironment
+    elif environment == "trio":
+        if not TrioEnvironment:
+            from .environments.trio import TrioEnvironment
+        env = TrioEnvironment
+    else:
+        raise NotImplementedError("Unkown environment: %r" % environment)
+    return env
